34fbd70c8eecee4655a733d96d0fc54497d4d1d6
Geometry Tryhard <memelite69@gmail.com>
2025-03-17 18:23:22 -0400

    update

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/tiw/',
-    bare:'https://erm.what-in.tf',
+    bare:'http://bareqwertyyuiopasdfghjklzxcvbnm.geo11.online/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
